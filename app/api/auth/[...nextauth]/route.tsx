8352c1237424c5d3f3bbd3d8bcd74b999cb5f98c
Aswin Lal M <aswnss.m@gmail.com>
2024-01-26 22:10:33 +0530

    next auth working

---
+++ app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,30 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: "jwt",
+    },
+    providers: [
+        CredentialsProvider({
+            name: "Sign in",
+            credentials: {
+                email: { label: "email", type: "email", placeholder: "johndoe@gmail.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+                if (user) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
+    ]
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
