efe132c36cbfda2559d654441475006e264774a8
Aswin Lal M <aswnss.m@gmail.com>
2024-01-27 09:03:14 +0530

    modified shadcn

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,8 +1,21 @@
+import LoginForm from "./_components/LoginForm";
+export default function Login() {
 
-export default async function Home() {
-  return (
-    <main>
-
-    </main>
-  );
+    return (
+        <div className="flex w-full h-screen">
+            <div className="w-full hidden lg:flex bg-gray-900 h-full p-5 items-center justify-center">
+                <div className="w-2/3">
+                    <h1 className="font-bold text-3xl tracking-tight">
+                        Welcome
+                    </h1>
+                    <p>
+                        Login to continue
+                    </p>
+                </div>
+            </div>
+            <div className="w-full h-full flex items-center  justify-center">
+                <LoginForm />
+            </div>
+        </div>
+    )
 }
